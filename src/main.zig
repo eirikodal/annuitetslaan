0583eefd09dea063b8d0a534cb8615dd759d9a8a
Eirik Øvstedal <eirikodal@gmail.com>
2026-01-18 21:25:36 +0100

    Farger og språk

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 const terminbelp_zig = @import("terminbelp_zig");
 
+const GREEN = "\x1b[32m";
+const RESET = "\x1b[0m";
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -9,7 +12,7 @@ pub fn main() !void {
     var args_iter = try std.process.argsWithAllocator(allocator);
     defer args_iter.deinit();
 
-    _ = args_iter.next(); // Skip program name
+    _ = args_iter.next(); // Skip program-navn
 
     var lan_str = args_iter.next();
     if (lan_str != null and (std.mem.eql(u8, lan_str.?, "-h") or std.mem.eql(u8, lan_str.?, "--help"))) {
@@ -25,15 +28,15 @@ pub fn main() !void {
         var buf: [100]u8 = undefined;
 
         if (lan_str == null) {
-            std.debug.print("Lanebelop: ", .{});
+            std.debug.print("Lånebeløp: ", .{});
             lan_str = try readLine(stdin, &buf, allocator);
         }
         if (ar_str == null) {
-            std.debug.print("Antall ar: ", .{});
+            std.debug.print("Antall år: ", .{});
             ar_str = try readLine(stdin, &buf, allocator);
         }
         if (rente_str == null) {
-            std.debug.print("Arlig rente (%): ", .{});
+            std.debug.print("Årlig rente (%): ", .{});
             rente_str = try readLine(stdin, &buf, allocator);
         }
     }
@@ -44,7 +47,7 @@ pub fn main() !void {
 
     const terminbelop = terminbelp_zig.calculate_terminbelop(lan, ar, rente);
 
-    std.debug.print("Terminbelop: {d:.2} kr/mnd\n", .{terminbelop});
+    std.debug.print("Terminbelop: " ++ GREEN ++ "{d:.2}" ++ RESET ++ " kr/mnd\n", .{terminbelop});
 }
 
 fn readLine(file: std.fs.File, buf: []u8, allocator: std.mem.Allocator) ![:0]const u8 {
